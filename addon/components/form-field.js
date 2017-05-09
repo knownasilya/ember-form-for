7008c234ecddc57c9e8d556a262b0646ee7835bb
Ilya Radchenko <knownasilya@gmail.com>
2017-05-09 15:07:23 -0400

    ConcatenatedProperties is an ember object that also has classNames and classNameBindings.. define a custom field

--- addon/components/form-field.js
+++ addon/components/form-field.js
@@ -47,13 +47,19 @@ const FormFieldComponent = Component.extend({
     'hintClasses',
     'errorClasses'
   ],
+  configClasses: [
+    'inputClasses',
+    'labelClasses',
+    'hintClasses',
+    'errorClasses'
+  ],
 
   control: 'one-way-input',
 
   init() {
     this._super(...arguments);
 
-    let concatenatedProperties = get(this, 'concatenatedProperties');
+    let configClasses = get(this, 'configClasses');
     let fieldClasses = get(this, 'config.fieldClasses');
     let classNames = this.classNames.compact();
     let classNameBindings = this.classNameBindings.compact();
@@ -62,7 +68,7 @@ const FormFieldComponent = Component.extend({
     this.classNameBindings = classNameBindings.slice();
     this.classNameBindings.push(`hasErrors:${get(this, 'config.fieldHasErrorClasses')}`);
 
-    concatenatedProperties.forEach((type) => {
+    configClasses.forEach((type) => {
       let result = get(this, `config.${type}`);
       let values = get(this, type) || [];
 
